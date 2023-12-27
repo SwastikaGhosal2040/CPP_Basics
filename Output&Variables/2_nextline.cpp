a4fea1e973f9c0c8f8ba6b39011f37a2c131833e
Swastika Ghosal <ghosalswastika2040@gmail.com>
2023-12-27 18:53:49 +0530

    Add files via upload

---
+++ Output&Variables/2_nextline.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //using endl
+    cout<<"Hello World"<<endl;
+    cout<<"Hi"<<endl;
+    cout<<endl<<endl;
+
+    //using \n
+    cout<<"Hello World!!!\n";
+    cout<<"Hi\n";
+
+}
