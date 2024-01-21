23184899f82bfb1c6a375449f5a336bc456691ed
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-21 23:58:15 +0530

    Add files via upload

---
+++ Input&Operators/sum.cpp
@@ -0,0 +1,14 @@
+//WAP to find the sum of 2 numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+int x;
+cout<<"Enter first number : ";
+cin>>x;
+int y;
+cout<<"Enter second number : ";
+cin>>y;
+int sum = x + y;
+cout<<"Sum of the two numbers : "<<sum;
+}
