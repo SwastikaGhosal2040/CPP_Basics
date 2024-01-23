1c66f72500272f42c178753fb66aef600f4389f9
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-23 14:32:03 +0530

    Add files via upload

---
+++ Input&Operators/operators.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x = 3;
+    cout<<(x==3);
+    cout<<(x==7);
+}
+
+/**
+ * Output:
+ * 10
+ * 
+ * Explanation:
+ * (x==3) ->condition true ->1
+ * (x==7) ->condition false->0
+ * As there is no new line answer is 10
+*/
