96cda371b580f8691f4c4e18a64c6a2a919afc81
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-23 13:56:59 +0530

    Add files via upload

---
+++ Input&Operators/BooleandataType.cpp
@@ -0,0 +1,14 @@
+//predict the output:
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x = 3;
+    int y = 3;
+    cout<<(x==y);
+}
+
+/**
+ * Output: 1
+ * (x==y) -> true -> 1
+*/
