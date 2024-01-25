58b828d3d20f7e457dda047368195d68ed5dc19f
Swastika Ghosal <ghosalswastika2040@gmail.com>
2024-01-25 23:55:47 +0530

    Add files via upload

---
+++ Input&Operators/char2.cpp
@@ -0,0 +1,13 @@
+//predict the output:
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch = 'ab';
+    cout<<ch;
+}
+
+/**
+ * Output:
+ *  multi-character character constant..shows warning
+*/
